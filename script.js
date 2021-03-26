96bc84071bafa8bed57adfdd62b199b911c7ae15
Фаниль Ганеев <wiper996100@gmail.com>
2021-03-26 20:53:00 +0600

    UpDate

--- script.js
+++ script.js
@@ -5,7 +5,5 @@ const nav = document.querySelector('.nav')
 background()
 
 function background() {
-    backgroundBlock.style.height = innerHeight + 'px'
-    img.style.width = innerWidth + 'px'
     nav.style.height = innerHeight + 'px'
 }    
